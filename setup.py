8e3a72e729b72239f8cee3c4fa817e26e91ea15e
Rajesh Kumar Bandaru <rkumar70900@gmail.com>
2020-05-19 21:35:51 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='matrix_operations',
+      version='0.1',
+      description='Matrix Operations',
+      packages=['matrix_operations'],
+      zip_safe=False)
